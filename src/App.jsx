c57cfbead6e0d21b934bf53b09f9f10983d38eec
BipinKoirala07 <111241615+BipinKoirala07@users.noreply.github.com>
2023-04-15 14:16:21 +0545

    Footer and story section added
    will continue tommorow

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import { useState,useEffect,memo } from 'react'
 import './App.css'
 import Header from './Components/header'
 import Footer from './Components/footer'
-// import Posts from './Components/posts'
+import Posts from './Components/posts'
+import UserStoriesCollection from "./Components/UserStoreiesCollection"
+
 
 function App() {
   const [posts , setPosts] = useState([])
 
-  useEffect(()=>{
-    fetch('https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"93024","first":12,"after":"XXXXXXXX"}')
-      .then(res => res.json())
-      .then(data=> console.log(data))
-  },[])
+  // useEffect(()=>{
+  //   fetch('https://www.instagram.com/graphql/query/?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"93024","first":12,"after":"XXXXXXXX"}')
+  //     .then(res => res.json())
+  //     .then(data=> console.log(data))
+  // },[])
 
   return (
     <div className="App">
       <Header />
-      {/*  <Posts /> */}
-      {/* <Footer /> */}
+      <UserStoriesCollection/>
+      <div className="all-posts">
+        <Posts/>
+      </div>
+      <Footer />
     </div>
   )
 }
