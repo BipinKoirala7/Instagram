bb69dfe67a12709ce42b92264b999b1c58283301
Bipinkoirala7 <bipinkoirala2061@gmail.com>
2023-04-21 16:33:47 +0545

    tailwind css setup added
    will be changed to all tailwind css tommorrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx}",
-    "./index.html.{html}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
